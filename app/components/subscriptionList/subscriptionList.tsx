44f32da96ee5c6e296eb2508377a2679d77462c6
lyla <bgy09270@naver.com>
2026-03-04 13:35:00 +0900

    hotfix: 그룹구독 뱃지 리스트에 추가(#4)

--- app/components/subscriptionList/subscriptionList.tsx
+++ app/components/subscriptionList/subscriptionList.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import Badge from "../badge/badge";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleRight } from "@fortawesome/free-solid-svg-icons/faAngleRight";
+import { faUserGroup } from "@fortawesome/free-solid-svg-icons";
 
 interface Props {
   href: string;
@@ -16,6 +17,8 @@ interface Props {
   billingCycle: "월간결제" | "연간결제";
   badgeLabel: string;
   badgeVariant: "primary" | "danger";
+  group?: boolean;
+  groupCount?: number;
 }
 
 export default function SubscriptionList({
@@ -27,10 +30,12 @@ export default function SubscriptionList({
   billingCycle,
   badgeLabel,
   badgeVariant,
+  group = false,
+  groupCount,
 }: Props) {
   const isFreeTrial = price === 0;
   return (
-    <div>
+    <li>
       <Link
         href={href}
         className="w-full grid grid-cols-[1fr_auto_auto] items-center gap-4 py-4 bg-white"
@@ -40,8 +45,15 @@ export default function SubscriptionList({
             <Image src={imageSrc} alt={imageAlt} width={40} height={40} />
           </div>
           <div>
-            <div className="text-base text-black font-bold leading-[150%]">
+            <div className="text-base text-black font-bold leading-[150%] flex gap-2 items-center">
               {name}
+
+              {group && (
+                <span className="text-brand-primary text-sm flex gap-1 items-center font-bold">
+                  <FontAwesomeIcon icon={faUserGroup} size="sm" />
+                  {groupCount}인
+                </span>
+              )}
             </div>
             <div className="item-subtext">
               <span>
@@ -61,6 +73,6 @@ export default function SubscriptionList({
           className="w-5 h-5 text-gray-300"
         />
       </Link>
-    </div>
+    </li>
   );
 }
