805d44144919cc70c0eae4a91ce71093a6ef6de3
김나영 <111120709+KNY1005@users.noreply.github.com>
2026-03-06 14:20:52 +0900

    fix : ai-analysis-banner 타입 추가

--- app/statistics/mock-subscriptions.ts
+++ app/statistics/mock-subscriptions.ts
@@ -20,51 +20,51 @@ export interface SubscriptionItem {
 
 // 2. 실제 목데이터 리스트
 export const MOCK_SUBSCRIPTIONS: SubscriptionItem[] = [
-  //  {
-  //    id: 1,
-  //    name: "넷플릭스",
-  //    type: "monthly",
-  //    billingDate: { day: 24 }, // 매달 24일 결제
-  //    endDate: "2026-12-31",
-  //    isShared: true,
-  //    sharedCount: 4,
-  //    paymentType: "paid",
-  //    price: 17000,
-  //    category: "OTT",
-  //  },
-  //  {
-  //    id: 2,
-  //    name: "유튜브 프리미엄",
-  //    type: "yearly",
-  //    billingDate: { month: 2, day: 24 }, // 매년 2월 24일 결제
-  //    endDate: "2027-02-24",
-  //    isShared: false,
-  //    paymentType: "paid",
-  //    price: 149000, // 연간 금액
-  //    category: "OTT",
-  //  },
-  //  {
-  //    id: 3,
-  //    name: "쿠팡 와우",
-  //    type: "monthly",
-  //    billingDate: { day: 10 },
-  //    endDate: "2026-08-10",
-  //    isShared: false,
-  //    paymentType: "free", // 무료 체험 중
-  //    price: 0,
-  //    freePeriod: 3, // 3개월 무료
-  //    category: "Shopping",
-  //  },
-  //  {
-  //    id: 4,
-  //    name: "스포티파이",
-  //    type: "monthly",
-  //    billingDate: { day: 15 },
-  //    endDate: "2026-11-15",
-  //    isShared: true,
-  //    sharedCount: 6,
-  //    paymentType: "paid",
-  //    price: 10900,
-  //    category: "Music",
-  //  },
+   {
+     id: 1,
+     name: "넷플릭스",
+     type: "monthly",
+     billingDate: { day: 24 }, // 매달 24일 결제
+     endDate: "2026-12-31",
+     isShared: true,
+     sharedCount: 4,
+     paymentType: "paid",
+     price: 17000,
+     category: "OTT",
+   },
+   {
+     id: 2,
+     name: "유튜브 프리미엄",
+     type: "yearly",
+     billingDate: { month: 2, day: 24 }, // 매년 2월 24일 결제
+     endDate: "2027-02-24",
+     isShared: false,
+     paymentType: "paid",
+     price: 149000, // 연간 금액
+     category: "OTT",
+   },
+   {
+     id: 3,
+     name: "쿠팡 와우",
+     type: "monthly",
+     billingDate: { day: 10 },
+     endDate: "2026-08-10",
+     isShared: false,
+     paymentType: "free", // 무료 체험 중
+     price: 0,
+     freePeriod: 3, // 3개월 무료
+     category: "Shopping",
+   },
+   {
+     id: 4,
+     name: "스포티파이",
+     type: "monthly",
+     billingDate: { day: 15 },
+     endDate: "2026-11-15",
+     isShared: true,
+     sharedCount: 6,
+     paymentType: "paid",
+     price: 10900,
+     category: "Music",
+   },
 ];
