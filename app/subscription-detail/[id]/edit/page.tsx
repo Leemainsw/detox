53f4612815374452361e81a848ba5e07ca818ca3
Leemainsw <yebom6476@gmail.com>
2026-03-11 20:43:00 +0900

    feat: loading 스크린 적용

--- app/subscription-detail/[id]/edit/page.tsx
+++ app/subscription-detail/[id]/edit/page.tsx
@@ -1,9 +1,10 @@
 import { Suspense } from "react";
 import EditPageContent from "./_components/edit-page-content";
+import LoadingScreen from "@/app/components/loading-screen";
 
 export default function Page() {
   return (
-    <Suspense fallback={<></>}>
+    <Suspense fallback={<LoadingScreen />}>
       <EditPageContent />
     </Suspense>
   );
