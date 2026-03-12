96fab4fa80afca1257f77f63d6035e9953e22801
Leemainsw <yebom6476@gmail.com>
2026-03-13 01:23:26 +0900

    fix: calculateTrial 일/개월 비교 로직 수정 (#41)

--- app/utils/subscriptions/calculateTrial.ts
+++ app/utils/subscriptions/calculateTrial.ts
@@ -1,7 +1,8 @@
+import { addMonths, isBefore, isEqual } from "date-fns";
+
 export default function calculateTrial(createdAt: string, trialMonths: number) {
   const today = new Date();
   const createdAtDate = new Date(createdAt);
-  const diffTime = Math.abs(today.getTime() - createdAtDate.getTime());
-  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-  return diffDays <= trialMonths;
+  const trialEndDate = addMonths(createdAtDate, trialMonths);
+  return isBefore(today, trialEndDate) || isEqual(today, trialEndDate);
 }
