ee37c5b5812199168a60f5f7e2bc180ef1897f71
Leemainsw <yebom6476@gmail.com>
2026-03-17 15:32:46 +0900

    refactor: calculateTrial parseISO (#41)

--- app/utils/subscriptions/calculateTrial.ts
+++ app/utils/subscriptions/calculateTrial.ts
@@ -1,8 +1,9 @@
-import { addMonths, isBefore, isEqual } from "date-fns";
+import { addMonths, isBefore, isEqual, parseISO } from "date-fns";
 
 export default function calculateTrial(createdAt: string, trialMonths: number) {
   const today = new Date();
-  const createdAtDate = new Date(createdAt);
+  const dateOnly = createdAt.split("T")[0];
+  const createdAtDate = parseISO(dateOnly);
   const trialEndDate = addMonths(createdAtDate, trialMonths);
   return isBefore(today, trialEndDate) || isEqual(today, trialEndDate);
 }
