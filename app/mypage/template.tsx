8aa28faeb412d489b77a574b2fa0f74bef0abb2c
배근영 <bgy09270@naver.com>
2026-03-19 11:35:49 +0900

    feat: 로그인페이지 모션적용, 모션중 스크롤 생성 방지 (#68)

--- app/mypage/template.tsx
+++ app/mypage/template.tsx
@@ -3,13 +3,9 @@
 import { motion } from "motion/react";
 import type { ReactNode } from "react";
 
-export default function MypageTemplate({
-  children,
-}: {
-  children: ReactNode;
-}) {
+export default function MypageTemplate({ children }: { children: ReactNode }) {
   return (
-    <div className="overflow-x-hidden">
+    <div className="overflow-hidden">
       <motion.div
         initial={{ opacity: 0, x: 20 }}
         animate={{ opacity: 1, x: 0 }}
